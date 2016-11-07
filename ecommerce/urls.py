f8670c1f860049169c74d3df2f22f4f7510ae54e
Jose Pablo <josepablo2692@gmail.com>
2016-11-07 15:32:38 -0600

    Lectura: Contexto dentro de Templates

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -7,6 +7,6 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'ecommerce.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-    url(r'^$', 'products.views.abc', name='home'),
+    url(r'^$', 'products.views.home', name='home'),
     url(r'^admin/', include(admin.site.urls)),
 )
--- products/views.py
+++ products/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
-def abc(request):
 
-	template = 'home.html'
-	context = locals()
-	return render(request, template, context) 
+def home(request):
+	if request.user.is_authenticated():
+		username_is = "José usando el contexto"
+		context = {"username_is": request.user}
+	else:
+		context = {"username_is": request.user}
+	
+	template = 'base.html'	
+	return render(request, template, context)
