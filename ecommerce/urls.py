5f9bdb679ea5011fe7397ab7661db1a0995cc35a
Jose Pablo <josepablo2692@gmail.com>
2016-11-07 19:18:15 -0600

    Documentos Estaticos - Statics Files

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -1,6 +1,8 @@
+from django.conf import settings
 from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
+from django.conf.urls.static import static
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -10,4 +12,10 @@ urlpatterns = patterns('',
     url(r'^$', 'products.views.home', name='home'),
     url(r'^products/$', 'products.views.all', name='products'),
     url(r'^admin/', include(admin.site.urls)),
+
 )
+
+
+if settings.DEBUG:
+	urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+	urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
