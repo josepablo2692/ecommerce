ee2cfbc3c6252a8fc1b79b7bb9ae76fd07f22d15
Jose Pablo <josepablo2692@gmail.com>
2016-11-08 12:38:59 -0600

    Página de producto I

--- products/views.py
+++ products/views.py
@@ -4,11 +4,26 @@ from django.shortcuts import render, Http404
 
 from .models import Product, ProductImage
 
-def home(request):
 
+def search(request):
+	try:
+		q = request.GET.get('q')
+	except:
+		q = None
+	
+	if q:
+		products = Product.objects.filter(title__icontains=q)
+		context = {'query': q, 'products': products}
+		template = 'products/results.html'	
+	else:
+		template = 'products/home.html'	
+		context = {}
+	return render(request, template, context)
+
+def home(request):
 	products = Product.objects.all()
 	template = 'products/home.html'	
-	context = {'products': products} 	
+	context = {"products": products}
 	return render(request, template, context)
 
 
@@ -16,19 +31,16 @@ def all(request):
 	products = Product.objects.all()
 	context = {'products': products}
 	template = 'products/all.html'	
-
 	return render(request, template, context)
 
+
 def single(request, slug):
-		try:
-				product = Product.objects.get(slug=slug)
-				images = ProductImage.objects.filter(product=product)
-				context = {'product': product, "images": images}
-				template = 'products/single.html'	
-				return render(request, template, context)
-		except:
-				raise Http404
-
-
-
-	
+	try:
+		product = Product.objects.get(slug=slug)
+		#images = product.productimage_set.all()
+		images = ProductImage.objects.filter(product=product)
+		context = {'product': product, "images": images}
+		template = 'products/single.html'	
+		return render(request, template, context)
+	except:
+raise Http404
